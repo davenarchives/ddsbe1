b071770b1b930d8bb4a42d6460abe35aa137a697
davenarchives <daventheabyss@gmail.com>
2025-03-18 00:09:54 +0800

    ddsbe1 final configuration

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+// Unsecured Routes
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/users', 'UserController@index'); // Get all users
+    $router->get('/users/{id}', 'UserController@show'); // Get user by ID
+    $router->post('/users', 'UserController@add'); // Create a new user
+    $router->put('/users/{id}', 'UserController@update'); // Update user
+    $router->patch('/users/{id}', 'UserController@update'); // Partial update
+    $router->delete('/users/{id}', 'UserController@delete'); // Delete user
+
+
+    $router->get('/usersjob', 'UserJobController@index');
+    $router->get('/usersjob/{id}', 'UserJobController@show');
+
+});
+
+// New Unsecured Routes without /api prefix (new addition)
+$router->group([], function () use ($router) {
+    $router->get('/users', 'UserController@index'); // Get all users (http://localhost:8000/users)
+    $router->get('/users/{id}', 'UserController@show'); // Get user by ID (http://localhost:8000/users/{id})
+    $router->post('/users', 'UserController@add'); // Create a new user
+    $router->put('/users/{id}', 'UserController@update'); // Update user
+    $router->patch('/users/{id}', 'UserController@update'); // Partial user update
+    $router->delete('/users/{id}', 'UserController@delete'); // Delete user
+
+    $router->get('/usersjob', 'UserJobController@index');
+    $router->get('/userjob/{id}', 'UserJobController@show');
+});
